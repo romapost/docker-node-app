125d11ce9929f3e5f1ec6fde81df40a848e9162c
Roman <roman.postovalov@gmail.com>
2020-11-22 22:00:16 +0100

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render(
+      'index',
+      {
+        title: 'Express app skeleton with Docker',
+        body: `Express is a minimal and flexible Node.js web application framework that 
+      provides a robust set of features for web and mobile applications.`
+      }
+  );
+});
+
+module.exports = router;
